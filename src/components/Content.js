c0e87e0b0fe8c0c897232090793cf895fac3b209
Rismafebriana <111462571+Rismafebriana@users.noreply.github.com>
2022-09-13 08:54:28 -0700

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,46 +1,45 @@
-import React from 'react';
+import React from "react";
 
-  function Content() {
-    return (
-       <>
+function Content() {
+	return (
+		<>
+			<div className="p-5 mb-4 bg-light rounded-3">
+				<div className="container-fluid py-5">
+					<h1 className="display-5 fw-bold">First Paragraph in Jumbotron</h1>
+					<p className="col-md-8 fs-4">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aperiam voluptatum molestias earum! Provident dolore odio molestiae
+						deserunt eos veniam. Lorem ipsum dolor sit amet consectetur adipisicing.
+					</p>
+					<button type="button" className="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#exampleModal">
+						Click here to show the Modal box
+					</button>
+				</div>
+			</div>
 
-        <div className="bg-light" id="about">
-          <div className="container-fluid py-5">
-            <h1 className='display-5 fw-bold'>FIRST PARAGRAF</h1>
-                <p className="mt-4 mb-5">Hacktiv8 adalah pendidikan yang berdiri pada tahun 2016.
-                Hacktiv8 didirikan oleh Roland Ishak dan Riza Fahmi, yang didasari oleh
-                keresahan mereka melihat banyaknya kejadian saling bajak talent-talent
-                pengembang di antara perusahaan-perusahaan teknologi. Hacktiv8 berfokus pada pengembangan
-                talent digital, mulai dari pemula hingga menjadi tenaga ahli lewat program bootcamp.
-                Dengan begitu diharapkan semakin banyak anak-anak muda yang berkompeten untuk berkarir
-                di dunia teknologi.</p>
-                <button className='btn btn-primary btn-lg' type='button' data-bs-toogle='modal' data-bs-target='#exampleModal'>
-                  Click here to show Modal box
-                </button>
-             </div>   
-          </div>
+			{/* Modal */}
+			<div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title">Modal title</h5>
+							<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+						</div>
+						<div className="modal-body">
+							<p>Ketimpa container</p>
+						</div>
+						<div className="modal-footer">
+							<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
+								Close
+							</button>
+							<button type="button" className="btn btn-primary">
+								Save changes
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
 
-          {/*modal*/}  
-           <div className='modal fade 'id='exampleModal' tabindex='-1' aria-labellebdy='exampleModalLabel' aria-hidden='true'>
-            <div className='modal-dialog'>
-              <div className='modal-content'>
-                <div className='modal-header'>
-                  <h5 className='modal-tittle' id='exampleModalLabel'>Modal tittle</h5>
-                  <button type='button' className='btn-close' data-bs-dismis='modal' aria-label='Close'>
-                  </button>
-                </div>
-                <div className='modal-body'>
-                  This is my first modal box
-                </div>
-                <div className='modalfooter'>
-                  <button type='button' className='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
-                  <button type='button' className='btn btn-primary'>Save Changes</button>
-                </div>
-              </div>
-            </div>
-           </div>
-   </>
-    );
-  }
-  
-  export default Content;
+export default Content;
