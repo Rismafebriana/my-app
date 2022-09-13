fee5f406588fe316143e36a62e4ded19d66ec71b
Rismafebriana <111462571+Rismafebriana@users.noreply.github.com>
2022-09-13 08:53:53 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,59 @@
-import React from 'react';
+import React from "react";
 
-function Header(){
-    return(
-        <>
-            <nav className="py-2 bg-light border-bottom">
-                <div className="container d-flex flex-wrap">
-                    <ul className="nav me-auto">
-                    <li className="nav-item">
-                    <a href="/" className="nav-link link-dark px-2 active" aria-current="page">HOME</a>
-                    </li>
-                <li className="nav-item"><a href="/" className="nav-link link-dark px-2">FEATURES</a></li>
-                <li className="nav-item"><a href="/" className="nav-link link-dark px-2">FAQS</a></li>
-                <li className="nav-item"><a href="/" className="nav-link link-dark px-2">ABOUT</a></li>
-                   </ul>
-                   <ul className="nav">
-                    <li className="nav-item"><a href="/" className="nav-link link-dark px-2">LOGIN</a></li>
-                    <li className="nav-item"><a href="/" className="nav-link link-dark px-2">SIGN UP</a></li>
-                   </ul>
-                </div>
-            </nav>
-            <header className='py-3 mb-4 border-bottom'>
-                <div className='container d-flex flex-wrap justify-content-center'>
-                    <a href='/' className='d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none'>
-                    <span className='fs-4'>My First React App</span>
-                    </a>
-                    <form className='col-12 col-lg-auto mb-3 mb-lg-0'>
-                        <input type='search' className ="form-control" placeholder='Search...' aria-label='Search'/>
-                    </form>
-                </div>
-            </header>
-        </>
-    )
+function Header() {
+	return (
+		<>
+			<nav className="py-2 bg-light border-bottom">
+				<div className="container d-flex flex-wrap">
+					<ul className="nav me-auto">
+						<li className="nav-item">
+							<a href="/" className="nav-link link-dark px-2 active">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="/" className="nav-link link-dark px-2">
+								Features
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="/" className="nav-link link-dark px-2">
+								FAQs
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="/" className="nav-link link-dark px-2">
+								About
+							</a>
+						</li>
+					</ul>
+					<ul className="nav">
+						<li className="nav-item">
+							<a href="/" className="nav-link link-dark px-2">
+								Login
+							</a>
+						</li>
+						<li className="nav-item">
+							<a href="/" className="nav-link link-dark px-2">
+								Sign Up
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+
+			<header className="py-3 mb-4 border-bottom">
+				<div className="container d-flex flex-wrap justify-content-center">
+					<a href="/" className="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto text-dark text-decoration-none">
+						<span className="fs-4">My First React App</span>
+					</a>
+					<form className="col-12 col-lg-auto mb-3 mb-lg-0">
+						<input type="search" className="form-control" placeholder="Search..." aria-label="search" />
+					</form>
+				</div>
+			</header>
+		</>
+	);
 }
 
 export default Header;
